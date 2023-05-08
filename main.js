b5fc7c30044b5483489a0134c79bdd25e40b0c15
Toha2003 <120188089+Toha2003@users.noreply.github.com>
2023-05-08 20:13:55 +0500

    asdasd

---
+++ main.js
@@ -0,0 +1,12 @@
+// let name=prompt("ismingizni kiriting");
+// let firstName=prompt("familyangizni kiriting");
+// let age = prompt("yoshingizni kiriting");
+// let manzil = prompt("yashash manzilingizni kiriting");
+// console.log();
+let obj = {
+    ismingiz:prompt("ismingizni kiriting"),
+    familiyangiz:prompt("Familyangizni kiriting"),
+    yoshingiz:prompt("yoshingizni kiriting"),
+    turarJpoyingiz:prompt("yashash manzilingiz"),
+}
+console.log(obj);
